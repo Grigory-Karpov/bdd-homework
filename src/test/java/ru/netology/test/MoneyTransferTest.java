5754f315d1ab77fe571ce86f24fc0f5f19719d3e
Grigory <gregkarpov64@gmail.com>
2025-12-21 18:12:56 +0400

    Final fix for test code

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
+package ru.netology.test;
 
-import com.codeborne.selenide.Configuration; // <--- ВАЖНО 1
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
@@ -12,24 +13,39 @@ class MoneyTransferTest {
 
     @Test
     void shouldTransferMoneyBetweenOwnCards() {
-        Configuration.headless = true; // <--- ВАЖНО 2: Включаем режим "без монитора"
+        // ЖЕЛЕЗОБЕТОННЫЕ НАСТРОЙКИ ДЛЯ GITHUB ACTIONS
+        Configuration.headless = true;  // Работаем без монитора
+        Configuration.timeout = 15000;  // Ждем загрузки элементов 15 секунд (вместо 4)
 
+        // 1. Открываем страницу (localhost работает, если приложение поднялось)
         var loginPage = open("http://localhost:9999", LoginPage.class);
+
+        // 2. Логинимся
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
+
+        // 3. Вводим код верификации
         var verificationCode = DataHelper.getVerificationCode();
         var dashboardPage = verificationPage.validVerify(verificationCode);
 
-        // ... дальше твой старый код ...
+        // 4. Получаем инфо о картах
         var firstCardInfo = DataHelper.getFirstCardInfo();
         var secondCardInfo = DataHelper.getSecondCardInfo();
+
+        // 5. Запоминаем балансы ДО
         var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getId());
         var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getId());
+
+        // 6. Переводим деньги (1000 р с первой на вторую)
         var amount = 1000;
         var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo.getId());
         dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+
+        // 7. Получаем балансы ПОСЛЕ
         var actualFirstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getId());
         var actualSecondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getId());
+
+        // 8. Сверяем
         assertEquals(firstCardBalance - amount, actualFirstCardBalance);
         assertEquals(secondCardBalance + amount, actualSecondCardBalance);
     }
