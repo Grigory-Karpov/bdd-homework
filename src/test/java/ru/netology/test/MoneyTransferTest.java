467e005231ff656fd47f69af1c3b40f36256b7fb
Grigory <gregkarpov64@gmail.com>
2025-12-21 17:40:25 +0400

    Enable headless mode in test

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,5 +1,6 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Configuration; // <--- ВАЖНО 1
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
@@ -11,35 +12,25 @@ class MoneyTransferTest {
 
     @Test
     void shouldTransferMoneyBetweenOwnCards() {
+        Configuration.headless = true; // <--- ВАЖНО 2: Включаем режим "без монитора"
+
         var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCode();
         var dashboardPage = verificationPage.validVerify(verificationCode);
 
-        // Получаем данные о картах
+        // ... дальше твой старый код ...
         var firstCardInfo = DataHelper.getFirstCardInfo();
         var secondCardInfo = DataHelper.getSecondCardInfo();
-
-        // Запоминаем баланс до перевода
         var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getId());
         var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getId());
-
-        // Сколько хотим перевести
         var amount = 1000;
-
-        // Выполняем перевод на вторую карту с первой
         var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo.getId());
         dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
-
-        // Проверяем баланс после перевода
         var actualFirstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getId());
         var actualSecondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getId());
-
-        // Проверки (Assertions)
-        // На первой должно убавиться
         assertEquals(firstCardBalance - amount, actualFirstCardBalance);
-        // На второй должно прибавиться
         assertEquals(secondCardBalance + amount, actualSecondCardBalance);
     }
 }
