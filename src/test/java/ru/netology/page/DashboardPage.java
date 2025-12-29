35a26734e12f431b2e3bcecff9c96be2961e17f8
Grigory <gregkarpov64@gmail.com>
2025-12-29 09:34:25 +0400

    Fix DashboardPage: remove lombok val

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -2,7 +2,6 @@ package ru.netology.page;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import lombok.val;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -11,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private final SelenideElement heading = $("[data-test-id=dashboard]");
-    private final ElementsCollection cards = $$(".list__item div"); // Берем список всех карт
+    private final ElementsCollection cards = $$(".list__item div");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
 
@@ -19,25 +18,21 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-    public int getCardBalance(String cardNumber) {
-        // Вырезаем последние 4 цифры из номера карты
-        String cardLast4 = cardNumber.substring(15);
-        // Ищем карту, в которой есть эти цифры
-        val text = cards.findBy(text(cardLast4)).text();
-        return extractBalance(text);
-    }
-
     public TransferPage selectCardToTransfer(String cardNumber) {
-        String cardLast4 = cardNumber.substring(15);
-        // Ищем кнопку "Пополнить" у нужной карты
-        cards.findBy(text(cardLast4)).$("button").click();
+        // Берем последние 4 цифры карты
+        cards.findBy(text(cardNumber.substring(15, 19))).$("button").click();
         return new TransferPage();
     }
 
+    public int getCardBalance(String cardNumber) {
+        String text = cards.findBy(text(cardNumber.substring(15, 19))).text();
+        return extractBalance(text);
+    }
+
     private int extractBalance(String text) {
-        val start = text.indexOf(balanceStart);
-        val finish = text.indexOf(balanceFinish);
-        val value = text.substring(start + balanceStart.length(), finish);
+        int start = text.indexOf(balanceStart);
+        int finish = text.indexOf(balanceFinish);
+        String value = text.substring(start + balanceStart.length(), finish);
         return Integer.parseInt(value);
     }
 }
