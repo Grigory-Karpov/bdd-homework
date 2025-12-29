be177bcf17f738e52eaecc78439e9186e620c7d5
Grigory <gregkarpov64@gmail.com>
2025-12-29 09:31:45 +0400

    Refactor: remove Lombok and fix tests

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,41 +1,67 @@
 package ru.netology.data;
 
-import lombok.Value;
-
 public class DataHelper {
     private DataHelper() {}
 
-    @Value
     public static class AuthInfo {
-        String login;
-        String password;
+        private final String login;
+        private final String password;
+
+        public AuthInfo(String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
 
-    @Value
     public static class VerificationCode {
-        String code;
+        private final String code;
+
+        public VerificationCode(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
     }
 
-    @Value
     public static class CardInfo {
-        String id;
-        String number;
-        String balance;
+        private final String id;
+        private final String number;
+        private final int balance;
+
+        public CardInfo(String id, String number, int balance) {
+            this.id = id;
+            this.number = number;
+            this.balance = balance;
+        }
+
+        public String getNumber() {
+            return number;
+        }
     }
 
     public static AuthInfo getAuthInfo() {
-        return new AuthInfo("vasya", "qwerty123");
+        return new AuthInfo("vasya", "qwerty1234"); // Данные из README (урок 2.4 BDD)
     }
 
-    public static VerificationCode getVerificationCode() {
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
         return new VerificationCode("12345");
     }
 
     public static CardInfo getFirstCardInfo() {
-        return new CardInfo("92df3f3c-a0dd-4852-a575-20a52c03c00c", "5559 0000 0000 0001", "10000");
+        return new CardInfo("92df3f1c-a033-48e6-8390-206f6b1f56c0", "5559 0000 0000 0001", 10000);
     }
 
     public static CardInfo getSecondCardInfo() {
-        return new CardInfo("0f3f5c2a-249e-4c3d-8287-09f7a039391d", "5559 0000 0000 0002", "10000");
+        return new CardInfo("0f3f5c2a-249e-4c3d-8287-09f7a039391d", "5559 0000 0000 0002", 10000);
     }
 }
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,40 +1,47 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
 
     @Test
     void shouldTransferMoneyBetweenOwnCards() {
-        var loginPage = open("http://127.0.0.1:9999", LoginPage.class);
+        var loginPage = open("http://localhost:9999", LoginPage.class);
 
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCode();
+
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         var dashboardPage = verificationPage.validVerify(verificationCode);
 
+        // Получаем данные карт
         var firstCardInfo = DataHelper.getFirstCardInfo();
         var secondCardInfo = DataHelper.getSecondCardInfo();
 
-        // ВАЖНО: Теперь передаем getNumber(), а не getId()
+        // Запоминаем баланс ДО перевода
         var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getNumber());
         var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getNumber());
+
         var amount = 1000;
 
-        // И здесь тоже getNumber()
+        // ПЕРЕВОД: Хотим перевести на ВТОРУЮ карту с ПЕРВОЙ.
+        // 1. Выбираем ВТОРУЮ карту (destination)
         var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo.getNumber());
+
+        // 2. Вводим сумму и номер ПЕРВОЙ карты (source)
         dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
 
-        // И здесь getNumber()
+        // Получаем баланс ПОСЛЕ перевода
         var actualFirstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getNumber());
         var actualSecondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getNumber());
 
-        assertEquals(firstCardBalance - amount, actualFirstCardBalance);
-        assertEquals(secondCardBalance + amount, actualSecondCardBalance);
+        // ПРОВЕРКИ
+        Assertions.assertEquals(firstCardBalance - amount, actualFirstCardBalance);
+        Assertions.assertEquals(secondCardBalance + amount, actualSecondCardBalance);
     }
 }
